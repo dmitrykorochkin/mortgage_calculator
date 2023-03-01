fc4a873cdc72ad1e83f22f71c4be8f5d76241e92
Dmitry Korochkin <abramslost@gmail.com>
2023-03-01 15:41:33 +0300

    add model.js

---
+++ js/controller.js
@@ -0,0 +1,6 @@
+import * as Model from "./model.js";
+
+window.onload  = function() {
+  const getData = Model.getData();
+
+};
---
+++ js/model.js
@@ -0,0 +1,15 @@
+const data = {
+  selectedProgram: 0.1,
+  programs: {
+    base: 0.1,
+    it: 0.047,
+    gov: 0.067,
+    zero: 0.12
+  }
+};
+
+function getData() {
+  return {...data};       /*Деструктуризация */
+}
+
+export {getData};
